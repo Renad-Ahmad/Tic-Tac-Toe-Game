2321b66c7455e181a58cde0adcaf74a546e01b7f
MAX <Renad-Ahmad>
2020-02-04 04:57:47 +0300

    user not be able to click the same square twice & the win for the rows

--- js/Tic-Tac-Toe.js
+++ js/Tic-Tac-Toe.js
@@ -1,47 +1,76 @@
-//initialization
-const gameArray  = new Array(3);     
+const gameArray = new Array(3);
 
-for (var i=0; i < gameArray.length; i++){         
-    gameArray[i]=new Array(3)
-}    
+for (let i = 0; i < gameArray.length; i++) {
+    gameArray[i] = new Array(3)
+}
 
 //initializeGame();
+var counter = 0;
+var player1Turn = true;
+var score = "tie";
 
-for (var i=0; i < gameArray.length; i++){       
-    for (var j=0; j < gameArray[i].length; j++){       
-        gameArray[i][j] = {status: "blank"}
+for (let i = 0; i < gameArray.length; i++) {
+    for (let j = 0; j < gameArray[i].length; j++) {
+        gameArray[i][j] = {
+            status: "blank"
+        }
     }
-} 
-
-var x = gameArray[1][1].status
-console.log(x)
+}
 
 
 $("#gameBoard").html("")
-for (var i=0; i < gameArray.length; i++){ 
+for (let i = 0; i < gameArray.length; i++) {
     $("#gameBoard").append('<tr id="row' + i + '" >')
-    for (var j=0; j < gameArray[i].length; j++)  { 
+    for (let j = 0; j < gameArray[i].length; j++) {
         $("#gameBoard").append('<td><img class="Square" id="rc' + i + '' + j + '" src="img/Blank.png" onclick="play(this.id)"></img></td>')
-    } 
+    }
     $("#gameBoard").append('</tr>')
-}  
+}
 
-
-let player1Turn = true;
-const play = function(cellId){
+//play function
+const play = function (cellId) {
     // extract i j
-    //if(gameArray[i][j].status == "blank"){
-        if(player1Turn){
-            document.getElementById(cellId).src='img/X.png';
+    let i = cellId.substring(2, 3);
+    let j = cellId.substring(3, 4);
+    //console.log(gameArray[i][j].status);
+    if (gameArray[i][j].status == "blank" ) {
+        if (player1Turn) {
+            gameArray[i][j].status = "x"
+            document.getElementById(cellId).src = 'img/X.png';
             player1Turn = !player1Turn;
-
-        }
-        else {
-            document.getElementById(cellId).src='img/O.png';
+        } else {
+            gameArray[i][j].status = "o"
+            document.getElementById(cellId).src = 'img/O.png';
             player1Turn = !player1Turn;
-
         }
-    //isGameOver()
-    //}
+
+        isGameOver()
+    }
+}
+
+
+
+const isGameOver = function () {
+    //check for row sequence
+    var isWin = false;
+
+    for(let i = 0; i < gameArray.length; i++){
+        console.log("i: " + i)
+        if( gameArray[i][0].status != "blank" 
+            && gameArray[i][0].status == gameArray[i][1].status
+            && gameArray[i][0].status == gameArray[i][2].status){
+            isWin = true;
+            console.log("winner: " + gameArray[i][0].status)
+            break;
+        }
+
+    }
+
+
+    //check for column sequence
+    
+    //check for diagonal sequence
+
+    //check for tie
 }
 
