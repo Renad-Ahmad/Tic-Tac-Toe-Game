dc6bba0549ffd22791ea41dfa61e570ddf75e0ee
MAX <Renad-Ahmad>
2020-02-05 13:49:58 +0300

    change the img links

--- js/Tic-Tac-Toe.js
+++ js/Tic-Tac-Toe.js
@@ -28,7 +28,7 @@ const initializeGame = function () {
     for (let i = 0; i < gameArray.length; i++) {
         $("#gameBoard").append('<tr id="row' + i + '" >')
         for (let j = 0; j < gameArray[i].length; j++) {
-            $("#gameBoard").append('<td><img class="Square" id="rc' + i + '' + j + '" src="img/Blank.png" onclick="play(this.id)"></img></td>')
+            $("#gameBoard").append('<td><img class="Square" id="rc' + i + '' + j + '" src="/img/Blank.png" onclick="play(this.id)"></img></td>')
         }
         $("#gameBoard").append('</tr>')
     }
@@ -48,12 +48,12 @@ const play = function (cellId) {
     if (gameArray[i][j].status == "blank" && !isGameOver) {
         if (player1Turn) {
             gameArray[i][j].status = "x"
-            document.getElementById(cellId).src = 'img/X.png';
+            document.getElementById(cellId).src = '/img/X.png';
             player1Turn = !player1Turn;
             $("h2").text("o turn");
         } else {
             gameArray[i][j].status = "o"
-            document.getElementById(cellId).src = 'img/O.png';
+            document.getElementById(cellId).src = '/img/O.png';
             player1Turn = !player1Turn;
             $("h2").text("x turn");
         }
